6018d734aea68f7ce5965709eec16c7f59da9ba2
ReyDemo <elizabethperlazamontano@gmail.com>
2022-09-27 22:00:56 +0000

    examen

---
+++ comparaEVOL.cpp
@@ -0,0 +1,26 @@
+ #include<iostream>
+using namespace std;
+int main() 
+{
+	float L,P;
+	cout<<"ingrese el primer numero que desea comparar:   ";
+	cin>> L;
+	cout<<"ingrese el segundo numero que desesa comparar:  ";
+	cin>> P;
+	if(L>P)
+	{
+		cout<<"es el primer numero es mayor:  ";
+
+	}
+	
+	else if(L==P)
+	{
+		cout<<"son iguales:  ";
+	}
+	else if(L<P)
+	{  
+		cout<<"el segundo nuemro es mayor:  ";
+	}
+
+	return(0);
+}
